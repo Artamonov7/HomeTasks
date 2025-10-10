deb8e3f1ef25917fa3b1d9cdb3169a27a0fe959f
artamonovai <artemmaster77555@gmail.com>
2025-10-10 23:12:34 +0300

    num 7

---
+++ task2/2_7.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <setjmp.h>
+
+jmp_buf begin;
+
+char curlex;
+
+void getlex(void);
+
+int expr(void);
+
+int add(void);
+
+int mult(void);
+
+void error();
+
+void getlex()
+{
+    while ((curlex = getchar()) == ' ');
+}
+
+void error(void)
+{
+    printf("\nОШИБКА!\n");
+    while (getchar() != '\n');
+    longjmp(begin, 1);
+}
+
+int expr()
+{
+    int e = add();
+    while (curlex == '+' || curlex == '-'){
+        int is_plus = (curlex == '+');
+        getlex();
+        if (is_plus)
+            e += add();
+        else
+            e -= add();
+    }
+
+    return e;
+}
+
+int add()
+{
+    int a = mult();
+    while (curlex == '*' || curlex == '/'){
+        int is_mult = (curlex == '*');
+        getlex();
+        if (is_mult)
+            a *= mult();
+        else
+            a /= mult();
+    }
+    return a;
+}
+
+int mult()
+{
+    int m;
+    switch (curlex){
+        case '0': case '1': case '2': case '3': case '4': case '5': 
+        case '6': case '7': case '8': case '9': m = curlex - '0';
+        break;
+        case '(': getlex(); 
+                m = expr();
+        if (curlex == ')') break;
+        default : error();
+    }
+    getlex();
+    if (curlex == '^'){
+        getlex();
+        int res = 1, i;
+        int n = mult();
+        if (n < 0)
+            error();
+        for (i = 0; i < n; i++)
+            res *= m;
+        return res;
+    }
+    return m;
+}
+
+int main(void)
+{
+    int result;
+    setjmp(begin);
+    printf("==>");
+    getlex();
+    result = expr();
+    if (curlex != '\n') error();
+    printf("\n%d\n", result);
+    return 0;
+}
+
